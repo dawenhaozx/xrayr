4ab8b08d42a52f9e22d2c56760bfced6e0f558f4
dawenhaozx <58341754+dawenhaozx@users.noreply.github.com>
2024-04-11 15:00:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"time"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/wyx2685/XrayR/cmd"
 )
 
 func main() {
+	time.Sleep(time.Duration(15-time.Now().Unix()%15) * time.Second)
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
